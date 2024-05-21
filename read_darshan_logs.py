5f9717c942b5ef767165cce6b8932520a307f852
Null <Null@MacBook-Air-4.local>
2024-05-20 22:22:14 -0500

    read darshan logs and print all the modules records

--- read_darshan_logs.py
+++ read_darshan_logs.py
@@ -19,9 +19,22 @@ with darshan.DarshanReport(darshan_file, read_all=True) as report:
     # print modules contained in the report
     print("modules: ", list(report.modules.keys()))
 
-    # export POSIX module records to DataFrame and print
-    posix_df = report.records["POSIX"].to_df()
-    print("POSIX df: ", posix_df)
+    for module in report.modules.keys():
+        print("/n -----------------------------------------/n")
+        print("module: ", module)
+        for record in report.records[module]:
+            record_df = record.to_df()
+            print("record: ", record_df)
+            print(type(record_df))
+            print(record_df.keys())
 
-    print(type(posix_df))
-    print(posix_df.keys())
+    # # export POSIX module records to DataFrame and print
+    # posix_df = report.records["POSIX"].to_df()
+    # print("POSIX df: ", posix_df)
+
+    # print(type(posix_df))
+    # print(posix_df.keys())
+
+    # # export H5F module records to DataFrame and print
+    # h5f_df = report.records["H5F"].to_df()
+    # print("H5F df: ", h5f_df)
