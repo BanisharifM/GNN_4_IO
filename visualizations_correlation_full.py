3b1f592511284ac594afd2a10610483ea6d835ad
NULL <msharif@iastate.edu>
2024-06-25 03:52:44 -0500

    upddate correlation visualization

--- visualizations_correlation_full.py
+++ visualizations_correlation_full.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load the data to get the list of attributes
-data = pd.read_csv('CSVs/sample_train_100.csv')
+data = pd.read_csv("CSVs/sample_train_100.csv")
 data = data.loc[:, (data != data.iloc[0]).any()]
 attributes = data.columns
 
@@ -12,21 +12,29 @@ attributes = data.columns
 visualization_dir = "results/correlation/visualization"
 os.makedirs(visualization_dir, exist_ok=True)
 
+
 # Function to create and save visualizations
-def create_visualizations(attribute, sorted_results):
+def create_visualizations(attribute, sorted_results, threshold=0.7):
+    # Filter attributes with Combined_Score greater than the threshold
+    filtered_sorted_results = sorted_results[
+        sorted_results["Combined_Score"] > threshold
+    ]
+
     # Bar chart of combined scores
     plt.figure(figsize=(10, 8))
-    sns.barplot(x="Combined_Score", y="attr2", data=sorted_results)
+    sns.barplot(x="Combined_Score", y="attr2", data=filtered_sorted_results)
     plt.title(f"Combined Correlation Scores for {attribute}")
     plt.xlabel("Combined Score")
     plt.ylabel("Attribute")
     plt.tight_layout()
+    for index, value in enumerate(filtered_sorted_results["Combined_Score"]):
+        plt.text(value, index, f"{value:.2f}")
     bar_chart_path = f"{visualization_dir}/combined_scores_bar_chart_{attribute}.png"
     plt.savefig(bar_chart_path)
     plt.close()
 
-    # Heatmap of Pearson and Spearman correlations
-    correlation_matrix = sorted_results[
+    # Heatmap of Pearson and Spearman correlations (excluding Combined_Score)
+    correlation_matrix = filtered_sorted_results[
         ["attr2", "Pearson_Correlation", "Spearman_Correlation"]
     ].set_index("attr2")
     plt.figure(figsize=(10, 8))
@@ -37,6 +45,7 @@ def create_visualizations(attribute, sorted_results):
     plt.savefig(heatmap_path)
     plt.close()
 
+
 # Generate visualizations for each attribute
 for attr in attributes:
     sorted_results_path = f"results/correlation/sorted/sorted_{attr}_correlations.xlsx"
@@ -44,4 +53,3 @@ for attr in attributes:
     create_visualizations(attr, sorted_results)
 
 print("Visualizations created and saved.")
-
