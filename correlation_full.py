93feec15141ae30285ad0f88eabb3541839983bb
NULL <msharif@iastate.edu>
2024-06-25 01:45:45 -0500

    update correlation including all counters

--- correlation_full.py
+++ correlation_full.py
@@ -5,28 +5,32 @@ from sklearn.feature_selection import mutual_info_regression
 file_path = "CSVs/sample_train_100.csv"
 data = pd.read_csv(file_path)
 
+# Remove constant columns (features with the same value in all samples)
+data = data.loc[:, (data != data.iloc[0]).any()]
+
 # Prepare a list to store the correlation results
 correlation_results = []
 
 # Calculate correlations for each pair of attributes
-for i, attr1 in enumerate(data.columns):
-    for attr2 in data.columns[i + 1 :]:
-        pearson_corr = data[attr1].corr(data[attr2], method="pearson")
-        spearman_corr = data[attr1].corr(data[attr2], method="spearman")
+for attr1 in data.columns:
+    for attr2 in data.columns:
+        if attr1 != attr2:
+            pearson_corr = data[attr1].corr(data[attr2], method="pearson")
+            spearman_corr = data[attr1].corr(data[attr2], method="spearman")
 
-        # For Mutual Information, we need to discretize continuous variables
-        mutual_info = mutual_info_regression(data[[attr1]], data[attr2])[0]
+            # For Mutual Information, we need to discretize continuous variables
+            mutual_info = mutual_info_regression(data[[attr1]], data[attr2])[0]
 
-        # Append the result to the list
-        correlation_results.append(
-            {
-                "Attribute1": attr1,
-                "Attribute2": attr2,
-                "Pearson_Correlation": pearson_corr,
-                "Spearman_Correlation": spearman_corr,
-                "Mutual_Information": mutual_info,
-            }
-        )
+            # Append the result to the list
+            correlation_results.append(
+                {
+                    "Attribute1": attr1,
+                    "Attribute2": attr2,
+                    "Pearson_Correlation": pearson_corr,
+                    "Spearman_Correlation": spearman_corr,
+                    "Mutual_Information": mutual_info,
+                }
+            )
 
 # Convert the list to a DataFrame
 correlation_results_df = pd.DataFrame(correlation_results)
