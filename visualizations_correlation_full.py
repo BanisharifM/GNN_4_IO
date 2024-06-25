5e0ac52cc71b8b0da24d3ebb9e1a7e5a41c938bb
NULL <msharif@iastate.edu>
2024-06-25 03:31:16 -0500

    save visualizations of correlation

--- visualizations_correlation_full.py
+++ visualizations_correlation_full.py
@@ -3,13 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Load the data to get the list of attributes
+data = pd.read_csv('CSVs/sample_train_100.csv')
+data = data.loc[:, (data != data.iloc[0]).any()]
+attributes = data.columns
+
 # Ensure the output directory exists
 visualization_dir = "results/correlation/visualization"
 os.makedirs(visualization_dir, exist_ok=True)
-
-# List of unique attributes
-attributes = data.columns
-
 
 # Function to create and save visualizations
 def create_visualizations(attribute, sorted_results):
@@ -36,7 +37,6 @@ def create_visualizations(attribute, sorted_results):
     plt.savefig(heatmap_path)
     plt.close()
 
-
 # Generate visualizations for each attribute
 for attr in attributes:
     sorted_results_path = f"results/correlation/sorted/sorted_{attr}_correlations.xlsx"
@@ -44,3 +44,4 @@ for attr in attributes:
     create_visualizations(attr, sorted_results)
 
 print("Visualizations created and saved.")
+
